66b340751445a7e0f62987c1afe0a4a8cbf1e9ac
Syam Iyer <iyersyam@gmail.com>
2021-08-01 20:50:43 +1000

    Set default port to 3000

--- src/app.local.ts
+++ src/app.local.ts
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.listen(port);
 console.log(`Listening on port ${port}...`);
